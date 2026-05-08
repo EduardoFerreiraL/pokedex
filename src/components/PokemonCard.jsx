f6e515edb06339c74b94400a79a1b1830db579c4
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-05-07 21:43:19 -0300

    style: Inicio de pokedex

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,30 @@
 function PokemonCard() {
   return (
     <div>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-        alt="Pikachu"
-      />
-      <h2>#025 — Pikachu</h2>
-      <p>Tipo: Elétrico</p>
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png"
+          alt="Infernape"
+        />
+        <h2>#392 — Infernape</h2>
+        <p>Tipo: Fire and Fighter</p>
+      </div>
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/715.png"
+          alt="Noivern"
+        />
+        <h2>#715 — Noivern</h2>
+        <p>Tipo: Dragon and Flying</p>
+      </div>
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/306.png"
+          alt="Aggron"
+        />
+        <h2>#306 — Aggron</h2>
+        <p>Tipo: Rock and Steel</p>
+      </div>
     </div>
   )
 }
