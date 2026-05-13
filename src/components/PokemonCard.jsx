a56f363e7fe8a0ac8b383a88d422216ebf3677f5
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-05-13 20:01:16 -0300

    feat: filtros

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,32 +1,47 @@
-function PokemonCard() {
-  return (
-    <div>
-      <div>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png"
-          alt="Infernape"
-        />
-        <h2>#392 — Infernape</h2>
-        <p>Tipo: Fire and Fighter</p>
-      </div>
-      <div>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/715.png"
-          alt="Noivern"
-        />
-        <h2>#715 — Noivern</h2>
-        <p>Tipo: Dragon and Flying</p>
-      </div>
-      <div>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/306.png"
-          alt="Aggron"
-        />
-        <h2>#306 — Aggron</h2>
-        <p>Tipo: Rock and Steel</p>
-      </div>
-    </div>
-  )
+function PokemonCard({ id, name, type, imageUrl }) {
+   return (
+     <div>
+      <img src={imageUrl} alt={name} />
+      <h2>
+        #{String(id).padStart(3, '0')} — {name}
+      </h2>
+      <p>Tipo: {type}</p>
+     </div>
+   )
 }
 
 export default PokemonCard
+
+
+// function PokemonCard() {
+//   return (
+//     <div>
+//       <div>
+//         <img
+//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png"
+//           alt="Infernape"
+//         />
+//         <h2>#392 — Infernape</h2>
+//         <p>Tipo: Fire and Fighter</p>
+//       </div>
+//       <div>
+//         <img
+//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/715.png"
+//           alt="Noivern"
+//         />
+//         <h2>#715 — Noivern</h2>
+//         <p>Tipo: Dragon and Flying</p>
+//       </div>
+//       <div>
+//         <img
+//           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/306.png"
+//           alt="Aggron"
+//         />
+//         <h2>#306 — Aggron</h2>
+//         <p>Tipo: Rock and Steel</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default PokemonCard
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,11 +1,61 @@
 import PokemonCard from './PokemonCard'
+import { useState } from 'react'
+
+const POKEMONS = [
+   {
+     id: 392,
+     name: 'Infernape',
+     type: 'Fogo/Lutador',
+     imageUrl:
+       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png',
+   },
+   {
+     id: 715,
+     name: 'Noiver',
+     type: 'Dragao/Voador',
+     imageUrl:
+       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/715.png',
+   },
+    {
+     id: 306,
+     name: 'Aggron',
+     type: 'Pedra/Metal',
+     imageUrl:
+       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/306.png',
+   },
+]
+
 
 function PokemonList() {
-  return (
-    <section>
-      <PokemonCard />
-    </section>
-  )
+
+  const [pokemons]= useState(POKEMONS)
+  const [filtro, setFiltro] = useState('')
+
+  const listaFiltrada = pokemons.filter((p) =>
+     p.name.toLowerCase().includes(filtro.toLowerCase()))
+
+   return (
+<section>
+       <label htmlFor="busca">Buscar por nome: </label>
+       <input
+          id="busca"
+          type="search"
+          value={filtro}
+          onChange={(e) => setFiltro(e.target.value)}
+          placeholder="Ex.: char"
+       />
+       {listaFiltrada.map((pokemon) => (
+          <PokemonCard 
+             key={pokemon.id}
+             id={pokemon.id}
+             name={pokemon.name}
+             type={pokemon.type}
+             imageUrl={pokemon.imageUrl} />
+       ))}
+     </section>
+
+   )
 }
+
 
 export default PokemonList
